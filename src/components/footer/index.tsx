f5f3f9606b72388ae2b8f15d1b6d1d9d166605b9
davi <daavisouza.santana@gmail.com>
2023-04-30 13:58:33 -0300

    feat: create footer

---
+++ src/components/footer/index.tsx
@@ -0,0 +1,17 @@
+export const Footer = () => {
+    return (
+        <footer>
+            <div>
+                <div>
+                    <a href="https://github.com/DaviSouzaS">
+                        <img src="../src/assets/github-logo.png" alt="github-logo" />
+                    </a>
+                    <a href="https://www.linkedin.com/in/davi-s-s/">
+                        <img src="../src/assets/linkedin-logo.png" alt="linkedin-logo" />
+                    </a>
+                </div>
+                <a href="https://docs.github.com/en/rest?apiVersion=2022-11-28">API do GitHub</a>
+            </div>
+        </footer>
+    )
+}
--- src/pages/InitalPage/index.tsx
+++ src/pages/InitalPage/index.tsx
@@ -1,3 +1,10 @@
+import { Footer } from "../../components/footer"
+
 export const InitalPage = () => {
-    return <h1>Inital Page</h1>
+    return (
+    <>
+    <h1>Inital Page</h1>
+    <Footer/>
+    </>
+    )
 }
