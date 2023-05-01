29d171ac56e8560f016fe14f1161373d5e7bd192
davi <daavisouza.santana@gmail.com>
2023-05-01 16:46:56 -0300

    feat: create search feature

--- src/components/SearchBar/index.tsx
+++ src/components/SearchBar/index.tsx
@@ -1,8 +1,10 @@
-export const SearchBar = () => {
+import { FieldValues } from "react-hook-form"
+
+export const SearchBar = ({register}: FieldValues) => {
     return (
         <div>
             <img src="../src/assets/search.svg" alt="search-icon" />
-            <input type="text" placeholder="Nome do usuário"/>
+            <input type="text" placeholder="Nome do usuário" {...register("search")}/>
         </div>
     )
 }
--- src/pages/InitalPage/index.tsx
+++ src/pages/InitalPage/index.tsx
@@ -1,10 +1,50 @@
 import { Footer } from "../../components/Footer";
+import { SearchBar } from "../../components/SearchBar";
+import { Button } from "../../components/Button";
+import * as yup from 'yup';
+import { FieldValues, useForm } from "react-hook-form";
+import { yupResolver } from '@hookform/resolvers/yup';
+import { useContext } from "react";
+import { UserContext } from "../../contexts/UserContext";
+
 
 export const InitalPage = () => {
+
+    const { getUserInfos, searchError, getUserRepositories }: any = useContext(UserContext)
+
+    const formSchema = yup.object().shape({
+        search: yup.string().required("Insira o nome de algum usuário"),
+    });
+
+    const { register, handleSubmit, formState: {errors} }: FieldValues = useForm({
+        resolver: yupResolver(formSchema)
+    });
+
+    const search = async (data:FieldValues) => {
+        await getUserInfos(data)
+        await getUserRepositories(data)
+    }
+
   return (
     <>
-      <h1>Inital Page</h1>
-      <Footer />
+        <div>
+            <img src="../src/assets/logo-gitfound.png" alt="gitfound-logo" />
+            <h1>Git Found</h1>
+        </div>
+
+        <form onSubmit={handleSubmit(search)}>
+            <SearchBar register={register}/>
+            {errors.search?.message}
+            {searchError === 404 && <p>Usuário não encontrado</p>}
+            <div>
+                <Button type="submit" content="Pesquisar"/>
+                <Button type="button" content="Histórico"/>
+            </div>
+        </form>
+
+        <p>Encontre usuários do Git Hub de maneira rápida</p>
+
+        <Footer/>
     </>
   );
 };
