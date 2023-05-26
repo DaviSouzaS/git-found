c69a30712631ccf62b7528dab971a5cc6b5ba22d
davi <daavisouza.santana@gmail.com>
2023-05-26 17:51:45 -0300

    fix: fixing footer path

--- src/pages/initialPage/index.tsx
+++ src/pages/initialPage/index.tsx
@@ -1,4 +1,4 @@
-import { Footer } from "../../components/Footer";
+import { Footer } from "../../components/footer";
 import { SearchBar } from "../../components/SearchBar";
 import { Button } from "../../components/Button";
 import * as yup from "yup";
@@ -10,14 +10,19 @@ import { HistoricModal } from "../../components/HistoricModal";
 import { iSearch } from "../../contexts/types";
 
 export const InitialPage = (): JSX.Element => {
-  const { searchError, openHistoricModal, openModal, search } = useContext(UserContext);
+  const { searchError, openHistoricModal, openModal, search } =
+    useContext(UserContext);
 
   const formSchema = yup.object().shape({
     search: yup.string().required("Insira o nome de algum usuário"),
   });
 
-  const { register, handleSubmit, formState: { errors } }: FieldValues = useForm<iSearch>({
-    resolver: yupResolver(formSchema)
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  }: FieldValues = useForm<iSearch>({
+    resolver: yupResolver(formSchema),
   });
 
   return (
@@ -25,28 +30,43 @@ export const InitialPage = (): JSX.Element => {
       {openModal && <HistoricModal />}
       <div className="flex flex-col">
         <div className="flex justify-center items-center mt-10">
-          <img className="w-32 sm:w-60" src="../src/assets/logo-gitfound.png" alt="gitfound-logo" />
-          <h1 className="text-white text-4xl sm:text-6xl font-inter font-bold">Git Found</h1>
+          <img
+            className="w-32 sm:w-60"
+            src="../src/assets/logo-gitfound.png"
+            alt="gitfound-logo"
+          />
+          <h1 className="text-white text-4xl sm:text-6xl font-inter font-bold">
+            Git Found
+          </h1>
         </div>
 
-        <form onSubmit={handleSubmit(search)} className="flex flex-col items-center mt-3">
+        <form
+          onSubmit={handleSubmit(search)}
+          className="flex flex-col items-center mt-3"
+        >
           <SearchBar register={register} />
           <p className="text-white p-1">{errors.search?.message}</p>
-          {searchError === 404 && <p className="text-white p-1">Usuário não encontrado</p>}
+          {searchError === 404 && (
+            <p className="text-white p-1">Usuário não encontrado</p>
+          )}
           <div className="mt-7 flex gap-8">
             <Button type="submit" content="Pesquisar" />
-            <Button type="button" content="Histórico" onclick={openHistoricModal}
+            <Button
+              type="button"
+              content="Histórico"
+              onclick={openHistoricModal}
             />
           </div>
         </form>
 
         <div className="flex justify-center mt-11">
-          <p className="text-white font-bold text-xs sm:text-base">Encontre usuários do Git Hub de maneira rápida</p>
+          <p className="text-white font-bold text-xs sm:text-base">
+            Encontre usuários do Git Hub de maneira rápida
+          </p>
         </div>
       </div>
 
-      <div >
-
+      <div>
         <Footer />
       </div>
     </div>
