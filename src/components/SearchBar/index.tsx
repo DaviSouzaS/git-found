14c793f8effd18430f062dd94faca342a9f580d1
davi <daavisouza.santana@gmail.com>
2023-05-27 00:48:59 -0300

    fix: fixing images paths

--- src/components/SearchBar/index.tsx
+++ src/components/SearchBar/index.tsx
@@ -33,8 +33,8 @@ export const SearchBar = ({register}: FieldValues): JSX.Element => {
     return (
         <div className='flex justify-around w-11/12 sm:w-4/5 h-12 bg-three rounded-3xl border max-w-3xl border-four'>
             
-            <img className='w-9 ml-1' src="../src/assets/search.svg" alt="search-icon" />
-
+            <svg className='w-9 ml-1' xmlns="http://www.w3.org/2000/svg" height="48" viewBox="0 96 960 960" width="48" fill="#30363D"><path d="M796 935 533 672q-30 26-69.959 40.5T378 727q-108.162 0-183.081-75Q120 577 120 471t75-181q75-75 181.5-75t181 75Q632 365 632 471.15 632 514 618 554q-14 40-42 75l264 262-44 44ZM377 667q81.25 0 138.125-57.5T572 471q0-81-56.875-138.5T377 275q-82.083 0-139.542 57.5Q180 390 180 471t57.458 138.5Q294.917 667 377 667Z"/></svg>
+            
             {buttonVoiceSearch ? 
             <input className='w-4/5 ml-1 bg-transparent focus:outline-none placeholder-four text-white' type="text" placeholder="Nome do usuário" {...register("search")}/> 
             : 
